c5fbcbf37ea7f3bb24c194ffb3dc6bed374a859d
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-03-23 22:44:22 +0000

    add cv

---
+++ js/button.js
@@ -0,0 +1,74 @@
+        var button = document.getElementById('button1');
+        button.onclick = function() {
+            var div = document.getElementById('click1');
+           
+            if (div.style.display !== 'none') {
+                div.style.display = 'none';
+            } else {
+                div.style.display = 'block';
+
+
+
+            }
+        };
+        var button2 = document.getElementById('button2');
+      button2.onclick = function() {
+        var div2 = document.getElementById('click2');
+         
+          if (div2.style.display !== 'none') {
+              div2.style.display = 'none';
+          } else {
+              div2.style.display = 'block';
+          }
+      };
+      var button3 = document.getElementById('button3');
+        button3.onclick = function() {
+          var div3 = document.getElementById('click3');
+           
+            if (div3.style.display !== 'none') {
+                div3.style.display = 'none';
+            } else {
+                div3.style.display = 'block';
+            }
+        };
+        var button4 = document.getElementById('button4');
+        button4.onclick = function() {
+          var div4 = document.getElementById('click4');
+           
+            if (div4.style.display !== 'none') {
+                div4.style.display = 'none';
+            } else {
+                div4.style.display = 'block';
+
+            }
+        };
+        var button5 = document.getElementById('button5');
+        button5.onclick = function() {
+          var div5 = document.getElementById('click5');
+           
+            if (div5.style.display !== 'none') {
+                div5.style.display = 'none';
+            } else {
+                div5.style.display = 'block';
+            }
+        };
+        var button6 = document.getElementById('button6');
+        button6.onclick = function() {
+          var div6 = document.getElementById('click6');
+           
+            if (div6.style.display !== 'none') {
+                div6.style.display = 'none';
+            } else {
+                div6.style.display = 'block';
+            }
+        };
+        var button7 = document.getElementById('button7');
+        button7.onclick = function() {
+          var div7 = document.getElementById('click7');           
+            if (div7.style.display !== 'none') {
+                div7.style.display = 'none';
+            } else {
+                div7.style.display = 'block';
+            }
+        };
+
